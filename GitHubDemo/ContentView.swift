6e16ee7adf743c76083387de515c72d475ad337a
Matthew DiNovo <matt.dinovo@icloud.com>
2024-12-09 14:53:50 -0600

    development : Added Rectangle

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -10,15 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
             Text("Version 1.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
+            
+            
+            Rectangle()
         }
         .padding()
-        .foregroundStyle(.green)
+        .foregroundStyle(.blue)
     }
 }
 
