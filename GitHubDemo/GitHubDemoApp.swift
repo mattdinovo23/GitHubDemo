f342dc1743ad4f2026dacbc78245612d475687ff
Matthew DiNovo <matt.dinovo@icloud.com>
2024-12-09 14:34:40 -0600

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Matt DiNovo on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
